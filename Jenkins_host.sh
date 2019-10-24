503db46181cfd751de6e50f89eace49ab83ff4cd
kristof-eekhaut <kristofeekhaut@gmail.com>
2019-10-24 22:27:51 +0200

    Update Jenkins_host.sh

--- Jenkins_host.sh
+++ Jenkins_host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
-amazon-linux-extras install java-openjdk11
+yum install java-11-openjdk-devel -y
 yum install git -y
 yum install maven -y	
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
