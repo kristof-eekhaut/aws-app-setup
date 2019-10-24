01af010ffa7d1bc8c525e4a606f422526816fa65
kristof-eekhaut <kristofeekhaut@gmail.com>
2019-10-24 22:23:40 +0200

    Create Jenkins_host.sh

---
+++ Jenkins_host.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install java-openjdk11
+yum install git -y
+yum install maven -y	
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install jenkins -y
+service jenkins start
