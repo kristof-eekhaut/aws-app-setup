15a21990d472d7a428122f6ad5286cc8435be899
kristof-eekhaut <kristofeekhaut@gmail.com>
2019-10-28 20:20:35 +0100

    Update Jenkins_master.sh

--- Jenkins_master.sh
+++ Jenkins_master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
+yum install git -y
 amazon-linux-extras install java-openjdk11 -y
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
