b34bd8c3ddc267fc7361ada7ea37eeb04110514d
kristof-eekhaut <kristofeekhaut@gmail.com>
2019-10-28 19:30:01 +0100

    Update Jenkins_host.sh

--- Jenkins_host.sh
+++ Jenkins_host.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 yum update -y
-yum install git -y
-yum install maven -y
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install jenkins -y
