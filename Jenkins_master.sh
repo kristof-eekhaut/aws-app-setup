c005463c40ce40c1680c2a2c91aee9b45a548a8d
kristof-eekhaut <kristofeekhaut@gmail.com>
2019-11-02 17:01:20 +0100

    Update Jenkins_master.sh

--- Jenkins_master.sh
+++ Jenkins_master.sh
@@ -5,4 +5,7 @@ amazon-linux-extras install java-openjdk11 -y
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install jenkins -y
+aws s3 sync --delete s3://kristof-eekhaut-jenkins-config /var/lib/jenkins 
 service jenkins start
+
+echo ' */1 *  *  *  * root       aws s3 sync --delete /var/lib/jenkins s3://kristof-eekhaut-jenkins-config' >> /etc/crontab
